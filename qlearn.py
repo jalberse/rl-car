0383fe619f411448a076f2beae366f765c7df4aa
jalberse <alberse.john@gmail.com>
2019-09-23 16:18:34 -0500

    Some state simplification

--- qlearn.py
+++ qlearn.py
@@ -9,13 +9,19 @@ np.set_printoptions(threshold=sys.maxsize)
 env = gym.make('CarRacing-v0')
 for i_episode in range(20):
     observation = env.reset()
-    for t in range(100):
+    t = 0
+    while(True):
+        t += 1
         env.render()
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
-        thresh = rgb_to_bw_threshold(observation)
-        cv.imshow("Observation",thresh)
+        state = rgb_to_bw_threshold(observation)
+        # TODO: Take this out when we train I think it might be slowing us down
+        cv.imshow("Observation",state)
         cv.waitKey(1)
+        # TODO: Terminating after 1000 timesteps rather than just when done or exiting playfield. Why?
+        #       Our local version of car_racing.py might differ from what I'm actually importing. 
+        #       Check out that code and how it's setting done
         if done:
             print("Episode finished after {} timesteps".format(t+1))
             break
