8b4be0a4899bf4c7a6d00b01d94b584ad746a63f
jalberse <alberse.john@gmail.com>
2019-09-25 17:16:09 -0500

    Remove gas and brake combo actions from action space

--- qlearn.py
+++ qlearn.py
@@ -8,15 +8,20 @@ from collections import defaultdict
 
 np.set_printoptions(threshold=sys.maxsize)
 
+ACTION_SPACE_SIZE = 9 # Number of possible actions
+
 def e_greedy(Q,state,epsilon):
     """
     Returns an action key given Q and state
-    An action key {0,..,11} is mapped to a possible action in T x G x B
+    An action key in {0,..,ACTION_SPACE_SIZE-1}, which is mapped to a possible action in T x G x B
         Returns the optimal action given Q, s with a probability of (1-epsilon)
         Returns a random action with a probability of epsilon
     """
     # TODO real implementation
-    return random.randint(0,11)
+    if (random.random() < 1-epsilon):
+        return np.argmax(Q[state])
+    else:
+        return random.randint(0,ACTION_SPACE_SIZE-1)
 
 # TODO: Each episode Terminating after 1000 timesteps rather than just when 
 #       done or exiting playfield. Why?
@@ -45,23 +50,20 @@ def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.
     # and brake   B = {0, 1} (brake or no brake)
     # This discretization should be OK - it is how controls for humans are discretized, after all
     #   Note: I think some literature on this environ also discretizes in this way. Check.
-    # Action space is therefore T x G x B (12 possible options)
-    Q = defaultdict(lambda: np.zeros(12)) # So Q[state] is a list of 12 values - a value for each of 12 actions in the state
+    # Action space is therefore T x G x B less situations where we have both brake and gas active (9 possible options)
+    Q = defaultdict(lambda: np.zeros(ACTION_SPACE_SIZE)) # So Q[state] is a list of 12 values - a value for each of 12 actions in the state
 
     # Maps action_key index to action the env can understand
     actions = [
-        [-1.,0.,0.],
-        [-1.,0.,1.],
-        [-1.,1.,0.],
-        [-1.,1.,1.],
         [0.,0.,0.],
         [0.,0.,1.],
         [0.,1.,0.],
-        [0.,1.,1.],
+        [-1.,0.,0.],
+        [-1.,0.,1.],
+        [-1.,1.,0.],
         [1.,0.,0.],
         [1.,0.,1.],
         [1.,1.,0.],
-        [1.,1.,1.],
     ]
 
     # Keep track of information we want to plot later
@@ -75,7 +77,6 @@ def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.
         t = 0
         # Get the initial state
         state = rgb_to_bw_threshold(observation) # state is a flattened 1-D tuple (hashable) from original np array
-        print(type(state))
         # Game loop
         while(True):
             t += 1
@@ -86,11 +87,6 @@ def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.
             observation, reward, done, info = env.step(actions[action_key])
             new_state = rgb_to_bw_threshold(observation)
 
-
-
-            #cv.imshow("State",state) # TODO delete for speed
-            #cv.waitKey(1)
-
             # Q(s,a) <- Q(s,a) + alpha[r + gamma*max_a'(Q(s',a')) - Q(s,a)]
             Q[state][action_key] = Q[state][action_key] + learning_rate*(reward + discount_rate*Q[new_state][np.argmax(Q[new_state])] - Q[state][action_key])
             
@@ -99,6 +95,9 @@ def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.
 
             # TODO update statistics dict
             # TODO plot as we go? save Q as we go?
+
+            # TODO Incrementally save the state in case training stops. Save Q and statistics in readable format.
+            #       This will also let us run the algo with a Q at a given snapshot and see it in action
 
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
