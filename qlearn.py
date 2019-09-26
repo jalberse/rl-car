8e931c051be58f7c3f5b36d640f13f557ce3351d
jalberse <alberse.john@gmail.com>
2019-09-25 19:07:27 -0500

    snapshots

--- qlearn.py
+++ qlearn.py
@@ -2,8 +2,13 @@ import gym
 import numpy as np
 import cv2 as cv
 import sys
+import os
+import datetime
+import json
+import pickle
 from preprocessing import rgb_to_bw_threshold
 import random
+import math
 from collections import defaultdict
 
 np.set_printoptions(threshold=sys.maxsize)
@@ -23,6 +28,10 @@ def e_greedy(Q,state,epsilon):
     else:
         return random.randint(0,ACTION_SPACE_SIZE-1)
 
+def default_action_generator():
+    # Replaces typical lambda for defaultdict so we may pickle it
+    return np.zeros(ACTION_SPACE_SIZE)
+
 # TODO: Each episode Terminating after 1000 timesteps rather than just when 
 #       done or exiting playfield. Why?
 #       Our local version of car_racing.py might differ from what
@@ -34,12 +43,20 @@ def e_greedy(Q,state,epsilon):
 #       plotting every n episodes so we at least have some plots if we can't
 #       finish the training run. Maybe a key to stop training and return Q immediately
 #       so we can save it at any time.
-def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.01, epsilon = 0.05):
+# TODO  reduce epsilon over time?
+def q_learning_train(env, num_episodes, snapshots_dir, discount_rate = 0.99, learning_rate = 0.01, epsilon = 0.05, snapshot_freq=1000):
     """
-    Note gamma is discount_rate, alpha is learning_rate
+    Returns Q(s,a), the value for all state, action pairs in S x A, after training with Q-learning with an epsilon-greedy policy
 
-    Returns Q(s,a), the value for all state, action pairs in S x A, after training with epsilon-greedy policy
+    env                 openAI gym environment (CarRacing-v0)
+    num_episodes        The number of episodes to train. TODO -1 to continue indefinately until we ctrl+c
+    discount_rate       gamma
+    learning_rate       alpha
+    epsilon             used in epsilon-greedy policy.
+    run_id              used to identify the run. Creates a snapshots folder using this id to store progress
+    snapshot_freq       Save Q, statistics every snapshot_freq episodes
     """
+
     # Initialize Q(s,a) for all s in S, a in A (mathematically)
     # In reality we initialize as each state S comes in (many states are unreachable, so don't waste time)
     
@@ -51,7 +68,7 @@ def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.
     # This discretization should be OK - it is how controls for humans are discretized, after all
     #   Note: I think some literature on this environ also discretizes in this way. Check.
     # Action space is therefore T x G x B less situations where we have both brake and gas active (9 possible options)
-    Q = defaultdict(lambda: np.zeros(ACTION_SPACE_SIZE)) # So Q[state] is a list of 12 values - a value for each of 12 actions in the state
+    Q = defaultdict(default_action_generator) # So Q[state] is a list of 12 values - a value for each of 12 actions in the state
 
     # Maps action_key index to action the env can understand
     actions = [
@@ -69,12 +86,13 @@ def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.
     # Keep track of information we want to plot later
     statistics = dict([
         ('rewards',np.zeros(num_episodes)), # Total reward obtained this episode
-        ('lap_time',np.zeros(num_episodes)), # Timesteps taken in episode
+        ('lap_times',np.zeros(num_episodes)), # Timesteps taken in episode
     ])
 
     for episode_cnt in range(num_episodes):
         observation = env.reset()
         t = 0
+        reward_total = 0
         # Get the initial state
         state = rgb_to_bw_threshold(observation) # state is a flattened 1-D tuple (hashable) from original np array
         # Game loop
@@ -93,17 +111,38 @@ def q_learning_train(env, num_episodes, discount_rate = 0.99, learning_rate = 0.
             # s <- s'
             state = new_state
 
-            # TODO update statistics dict
-            # TODO plot as we go? save Q as we go?
-
-            # TODO Incrementally save the state in case training stops. Save Q and statistics in readable format.
-            #       This will also let us run the algo with a Q at a given snapshot and see it in action
+            # Reward across the whole episode
+            reward_total += reward
 
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
+                # Update statistics for the episode
+                statistics['rewards'][episode_cnt] = reward_total
+                statistics['lap_times'][episode_cnt] = t+1
+                if (episode_cnt % snapshot_freq == 0 and episode_cnt != 0):
+                    print(f'saving snapshot files to {snapshots_dir}/{episode_cnt:10d}_*.json')
+                    save_snapshot(Q,statistics,snapshots_dir,f'{episode_cnt:010d}')
+                # break to next episode
                 break
 
     return Q, statistics
+
+def save_snapshot(Q,statistics,directory,filename_prefix):
+    # Saves a snapshot to the given directory with the given filename prefix
+    # Saves Q in its own file (very large) and statistics in another
+    data = {
+        'rewards': statistics['rewards'].tolist(),
+        'lap_times': statistics['lap_times'].tolist(),
+    }
+    
+    if (os.path.isdir(directory)):
+        with open(os.path.join(directory,f'{filename_prefix}_Q.pkl'),'wb') as f:
+            pickle.dump(Q, f, pickle.HIGHEST_PROTOCOL)
+        with open(os.path.join(directory,f'{filename_prefix}_statistics.json'),'w+') as outfile:
+            json.dump(data, outfile)
+    else:
+        print(f'save_snapshot(): not such file or directory. Snapshot not saved.')
+    pass
 
 if __name__ == '__main__':
     env = gym.make('CarRacing-v0')
@@ -111,10 +150,19 @@ if __name__ == '__main__':
     discount_rate = 0.99
     learning_rate = 0.01
     epsilon = 0.05
+    snapshot_freq = 1000
+
+    now = datetime.datetime.now()
+    timestamp = f'{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.microsecond}'
+
+    snapshots_dir = f'snapshots/snapshots_{timestamp}'
+    os.mkdir(snapshots_dir)
 
     # Train the model
-    Q, statistics = q_learning_train(env,episodes,discount_rate,learning_rate,epsilon)
+    Q, statistics = q_learning_train(env,episodes,snapshots_dir,discount_rate=discount_rate,learning_rate=learning_rate,epsilon=epsilon,snapshot_freq=snapshot_freq)
+
+    # Save a snapshot of the model and statistics    
+    save_snapshot(Q,statistics,snapshots_dir,'FINAL')
+    print(f'Final snapshot saved to {snapshots_dir}/FINAL*')
 
     env.close()
-
-    # TODO make some plots
